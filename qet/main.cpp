edee3d7c0e8f2d3a47f4c6a297dbea1d572e1587
Antony Searle <antony.searle@gmail.com>
2024-03-19 20:15:43 +1100

    Initial Commit

---
+++ qet/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  qet
+//
+//  Created by Antony Searle on 19/3/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
